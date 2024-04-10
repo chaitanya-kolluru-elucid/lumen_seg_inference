6d83a3023c78de889244c3dd1bba6c568ef1a021
chaitanya-kolluru-elucid <chaitanya.kolluru@elucid.com>
2024-04-10 04:38:44 +0000

    Add stub for inference pipeline script

---
+++ run_inference_pipeline.py
@@ -0,0 +1,31 @@
+import glob
+import os
+import itk
+
+from preprocessing import preprocess
+from inferencing import infer
+from postprocessing import postprocess
+
+if __name__ == '__main__':
+
+    image_filelist = sorted(glob.glob(os.path.join('images', '*.nii.gz')))
+    
+    for k in range(len(image_filelist)):
+
+        case_name = os.path.basename(image_filelist[k]).split('_0000')[0]
+
+        # Get the image
+        im = itk.imread(image_filelist[k])
+
+        # Run preprocessing steps
+        preprocessed = preprocess(im)
+
+        # Run inference steps
+        prediction = infer(preprocessed)
+
+        # Run postprocessing steps
+        postprocessed = postprocess(prediction)
+
+        # Save the result
+        itk.imwrtie(postprocessed, os.path.join('predictions', case_name + '.nii.gz'))
+
